f0c5607cdf6f2e4ba7a5890451da82429d18ca92
James Meyers <James.Meyers919@gmail.com>
2015-07-23 16:45:43 -0700

    added files

---
+++ README.md
@@ -0,0 +1,40 @@
+# Experimenting with higher order React components
+
+
+### Stream Listener 
+`(stream, [propKey]) => (ReactComponent) => WrappedReactComponent`
+
+Takes a stream and an optional prop key. When the stream fires an event, the new value is stored in the wrapper components state and then passed to the child component as a property. A couple of uses for this are dealing with window resizes and hash changes:
+
+```javascript
+// get window dimensions
+function getWindowDims() {
+  return {
+    windowWidth: window.innerWidth,
+    windowHeight: window.innerHeight
+  };
+}
+
+// Currently streamListener only support flyd streams (https://github.com/paldepind/flyd)
+const dimensionStream = flyd.stream(getWindowDims());
+
+// when the window resizes, push the new dimensions into the stream
+window.addEventListener("resize", () => dimensionStream(getWindowDims()));
+
+@streamListener(dimensionStream)
+class ResizingBox extends React.Component {
+  render() {
+      const {windowWidth, windowHeight} = this.props;
+        const style = {
+          width: windowWidth * 0.5, 
+            height: windowHeight * 0.5
+        };
+        return (
+          <div style={style}>
+              {this.props.children}
+            </div>
+        );
+    }
+}
+
+```
---
+++ gulpfile.babel.js
@@ -0,0 +1,67 @@
+import {extend} from "lodash";
+import browserify from "browserify";
+import browserifyInc from "browserify-incremental";
+import babelify from "babelify";
+import express from "express";
+import fs from "fs";
+import gulp from "gulp";
+import sass from "gulp-ruby-sass";
+const port = 8080;
+
+gulp.task("babel", () => {
+  const b = browserify("./lib/app.js", extend({
+    debug: true,
+    paths: ["./node_modules", "./lib"]
+  }, browserifyInc.args));
+
+  // compile ES6/JSX
+  b.transform(babelify.configure({
+    ignore: /node_modules/,
+    stage: 1
+  })).on("error", function(error) {
+    console.log(`Babel Error: ${error}`);
+    this.emit("end"); // keeps task from hanging on error
+  });
+
+  // Cache
+  browserifyInc(b, {cacheFile: "./.browserify-cache"});
+
+  return b
+    .bundle()
+    .pipe(fs.createWriteStream("./bin/app.js"));
+});
+
+gulp.task("sass", () => {
+  return sass("./lib/style/app.scss")
+    .on("error", (error) => console.log(`Sass Error: ${error}`))
+    .pipe(gulp.dest("./bin"));
+});
+
+gulp.task("html", () => {
+  return gulp.src("./lib/index.html")
+    .pipe(gulp.dest("./bin"));
+});
+
+gulp.task("images", () => {
+  return gulp.src("./images/**/*")
+    .pipe(gulp.dest("./bin"));
+});
+
+gulp.task("watch", () => {
+  gulp.watch("./lib/**/*.?(js|json)", ["babel"]);
+  gulp.watch("./lib/style/**/*.scss", ["sass"]);
+  gulp.watch("./lib/index.html", ["html"]);
+});
+
+gulp.task("serve", () => {
+  const app = express();
+  app.use(express.static("./bin"));
+  app.use(express.static("./node_modules"));
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+});
+
+gulp.task("build", ["babel", "sass", "html", "images"]);
+
+gulp.task("default", ["build", "watch", "serve"]);
